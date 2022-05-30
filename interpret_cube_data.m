590b51849b370ba53b50704ee2345f57b06aa356
Angel Rodes <53089531+angelrodes@users.noreply.github.com>
2022-05-30 16:04:43 +0200

    Add files via upload

---
+++ interpret_cube_data.m
@@ -0,0 +1,46 @@
+clear
+close all hidden
+
+load('solution.mat')
+
+scale=solution.shielding(1);
+
+% interpret sides
+sel=solution.sample>1 & solution.sample<6;
+x=solution.side(sel)/scale;
+y=solution.shielding(sel);
+% side formula: y=b+(1-b)*exp(-x*a)
+b=mean(y(x>6));
+a=median(log((y-b)/(1-b))./(-x));
+
+figure
+hold on
+title('side')
+xlabel('size (m)')
+ylabel('shielding factor')
+plot(x,y,'.k')
+ymodel=b+(1-b)*exp(-x*a);
+plot(x,ymodel,'-r')
+text(mean(x), mean(ymodel),...
+    [num2str(b) '+' num2str((1-b)) '*exp(-x*' num2str(a) ')'],...
+    'Color','r')
+
+% interpret bottom
+sel=solution.sample==6;
+x=solution.side(sel)/scale;
+y=solution.shielding(sel);
+% side formula: y=b+(1-b)*exp(-x*a)
+b=0;
+a=median(log((y-b)/(1-b))./(-x));
+
+figure
+hold on
+title('bottom')
+xlabel('size (m)')
+ylabel('shielding factor')
+plot(x,y,'.k')
+ymodel=b+(1-b)*exp(-x*a);
+plot(x,ymodel,'-r')
+text(mean(x), mean(ymodel),...
+    [num2str(b) '+' num2str((1-b)) '*exp(-x*' num2str(a) ')'],...
+    'Color','r')
